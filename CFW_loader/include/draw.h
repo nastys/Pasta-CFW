285cf9dfff750c99890041ccd9eb4344b4df5064
AlbertoSONIC <albertosonic9000@gmail.com>
2015-06-20 09:44:57 +0200

    Now ui-development is up-to-date with master changes

--- CFW_loader/include/draw.h
+++ CFW_loader/include/draw.h
@@ -2,38 +2,38 @@
 
 #include "common.h"
 
-#define BYTES_PER_PIXEL 3
+//It used 24 bits color, so 1 pixel = 3 Bytes.
+#define SCREEN_BYTES_PER_PIXEL 3
 #define SCREEN_WIDTH 240
 #define SCREEN_HEIGHT 400
-
+//This is the buffer area size of a screen in bytes.
 #define SCREEN_SIZE (BYTES_PER_PIXEL * SCREEN_WIDTH * SCREEN_HEIGHT)
 
+// @breif  Construct a RGB value.
+// @param  r, g, b. Red, Green, Blue.
+// @retval a u32 RGB value.
 #define RGB(r,g,b) (r<<24|b<<16|g<<8|r)
 
-#define TOP_SCREEN0 (u8*)(0x20000000)
-#define TOP_SCREEN1 (u8*)(0x20046500)
-#define BOT_SCREEN0 (u8*)(0x2008CA00)
-#define BOT_SCREEN1 (u8*)(0x200C4E00)
+#define SCREEN_AREA_TOP0 (u8*)(0x20000000)
+#define SCREEN_AREA_TOP1 (u8*)(0x20046500)
+#define SCREEN_AREA_BOT0 (u8*)(0x2008CA00)
+#define SCREEN_AREA_BOT1 (u8*)(0x200C4E00)
+extern u32 drawInternalY;
 
-#define FB_TOP_LEFT1_FCRAM  (0x20184E60)
-#define FB_TOP_LEFT2_FCRAM  (0x201CB370)
-#define FB_TOP_RIGHT1_FCRAM (0x20282160)
-#define FB_TOP_RIGHT2_FCRAM (0x202C8670)
-#define FB_BOT_1_FCRAM      (0x202118E0)
-#define FB_BOT_2_FCRAM      (0x20249CF0)
+void DrawClearScreen(u8 *screenArea, int color);
+void DrawClearScreenAll(void);
 
-extern int current_y;
-
-void ClearScreen(unsigned char *screen, int color);
+//void DrawCharacter(u8 *screenArea, const char character, u32 x, u32 y, u32 foreColor, u32 backColor); //buggy
 void DrawCharacter(unsigned char *screen, int character, int x, int y, int color, int bgcolor);
-void DrawHex(unsigned char *screen, unsigned int hex, int x, int y, int color, int bgcolor);
-void DrawString(unsigned char *screen, const char *str, int x, int y, int color, int bgcolor);
-void DrawStringF(int x, int y, const char *format, ...);
-void DrawHexWithName(unsigned char *screen, const char *str, unsigned int hex, int x, int y, int color, int bgcolor);
+void DrawString(u8 *screenArea, const char *str, u32 x, u32 y, u32 foreColor, u32 backColor);
+void DrawDebug(u8 newline, const char *format, ...);
 
-void Debug(const char *format, ...);
-void DebugNoNewLine(const char *format, ...);
-void SplashScreen();
+void DrawTopSplash(char splash_file[]);
+void DrawBottomSplash(char splash_file[]);
 
-void drawTop(char splash_file[]);
-void drawBottom(char splash_file[]);
+//Unused.
+void DrawStringFormat(u8 newline, u8 debug, u32 x, u32 y, const char *format, ...);
+
+//Un-implemented.
+//void DrawHex(uint8_t *screenArea, unsigned int hex, u32 x, u32 y, u32 foreColor, u32 backColor);
+//void DrawHexWithName(uint8_t *screenArea, const char *str, unsigned int hex, u32 x, u32 y, u32 foreColor, u32 backColor);
